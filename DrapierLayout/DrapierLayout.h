ec4b047cfb9714b59a680ae332f52a71e2c7771d
jkolb <franticapparatus@gmail.com>
2014-10-30 22:36:38 -0500

    Initial Commit

---
+++ DrapierLayout/DrapierLayout.h
@@ -0,0 +1,19 @@
+//
+//  DrapierLayout.h
+//  DrapierLayout
+//
+//  Created by Justin Kolb on 10/30/14.
+//  Copyright (c) 2014 Justin Kolb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DrapierLayout.
+FOUNDATION_EXPORT double DrapierLayoutVersionNumber;
+
+//! Project version string for DrapierLayout.
+FOUNDATION_EXPORT const unsigned char DrapierLayoutVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DrapierLayout/PublicHeader.h>
+
+
